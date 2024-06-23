30d93e9ffd8ee4bcb85c74a6e0cf8d9e41b7b21f
qiang.liang <lq173228514@gmail.com>
2024-06-23 15:10:58 +0800

    优化防重复提交逻辑

--- src/main/java/com/hdfk7/module/aspect/ResubmitCheckAspect.java
+++ src/main/java/com/hdfk7/module/aspect/ResubmitCheckAspect.java
@@ -1,21 +1,18 @@
 package com.hdfk7.module.aspect;
 
 import com.hdfk7.proto.base.annotation.ResubmitCheck;
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ResubmitCheckAspect extends com.hdfk7.boot.starter.common.aspect.ResubmitCheckAspect {
-    @Before("@within(resubmitCheck)")
-    public void adviceClazz(JoinPoint joinPoint, ResubmitCheck resubmitCheck) {
-        doAdvice(joinPoint, resubmitCheck);
+
+    @Around("@within(resubmitCheck) || @annotation(resubmitCheck)")
+    public Object doAround(ProceedingJoinPoint joinPoint, ResubmitCheck resubmitCheck) throws Throwable {
+        return doTask(joinPoint, resubmitCheck);
     }
 
-    @Before("@annotation(resubmitCheck)")
-    public void adviceMethod(JoinPoint joinPoint, ResubmitCheck resubmitCheck) {
-        doAdvice(joinPoint, resubmitCheck);
-    }
 }
